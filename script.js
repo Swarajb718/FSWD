cfaa0727a14624e6b8ab40645ae5a7de24708e55
Swarajb718 <swarajb718@gmail.com>
2025-05-25 22:57:34 +0530

    ad

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let a = 87
-c=78
-console.log(a>c)
+// let a = 87
+// c=78
+// console.log(a>c)
+// console.log(a>c)
+// console.log(a>c)
+// let a = 20;
+// let b = 30;NO
+// console.log(a,b);
+
+let arr = [10,20,30,40,50]
+arr.forEach((ele)=>{
+    console.log(ele);
+})
+hsdbjds
